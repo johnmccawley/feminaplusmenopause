d4a0cf227f21e6b21828f2390506c79ee1785675
Sean Baluha <sbaluha@jekyllhydelabs.com>
2016-07-25 16:23:10 -0400

    All information needed on receipt page now there

--- resources/views/receipt.blade.php
+++ resources/views/receipt.blade.php
@@ -5,10 +5,38 @@
 
 <?php
     $page_id="receipt";
+    $billing_address_2 = ($request->input('billing-address-2')) ? $request->input('billing-address-2') : null;
+    $shipping_address_2 = ($request->input('shipping-address-2')) ? $request->input('shipping-address-2') : null;
 ?>
 
 @section('content')
     <section id="tile-cart">
+        <div>
+            THANK YOU FOR PURCHASING FEMINA PLUS!
+        </div>
+        <div class="container">
+            Billing Information:
+            First Name: {{ $request->input('billing-name-first') }}
+            Last Name: {{ $request->input('billing-name-last') }}
+            Email: {{ $request->input('billing-email') }}
+            Phone: {{ $request->input('billing-phone') }}
+            Address One: {{ $request->input('billing-address-1') }}
+            Address Two: {{ $billing_address_2 }}
+            City: {{ $request->input('billing-city') }}
+            State: {{ $request->input('billing-state') }}
+            Zip: {{ $request->input('billing-zip') }}
+            <br />
+            Shipping Information:
+            First Name: {{ $request->input('shipping-name-first') | $request->input('billing-name-first') }}
+            Last Name: {{ $request->input('shipping-name-last') | $request->input('billing-name-last')}}
+            Email: {{ $request->input('shipping-email') | $request->input('billing-email') }}
+            Phone: {{ $request->input('shipping-phone') | $request->input('billing-phone') }}
+            Address One: {{ $request->input('shipping-address-1') | $request->input('billing-address-1') }}
+            Address Two: {{ $shipping_address_2 }}
+            City: {{ $request->input('shipping-city') | $request->input('billing-city') }}
+            State: {{ $request->input('shipping-state') | $request->input('billing-state') }}
+            Zip: {{ $request->input('shipping-zip') | $request->input('billing-zip') }}
+        </div>
         <div class="container">
             <div class="row cart-header">
                 <div class="span3 item-name">
