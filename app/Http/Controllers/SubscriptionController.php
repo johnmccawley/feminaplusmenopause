d680b0b1bb08b97b8d05fa20bbb8490657d0b5d6
Sean Baluha <sbaluha@jekyllhydelabs.com>
2016-08-18 15:44:45 -0400

    WIP

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -53,7 +53,18 @@ class SubscriptionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $plan = Plan::retrieve('fpClub');
+        $displayTotal = $this->formatDisplayPrice($plan->amount);
+        $cartItems = json_decode("{'fpClub':{'amount':1,'type':'plan','name':'Femina Plus Club Refill','description':'1 Bottle a Month for 12 Months (13th Bottle Free!)','price':$plan->amount,'display_price':$displayTotal}}");
+        $sessionToken = $request->session()->get('_token');
+        $transactionId = ($request->input('paymentId')) ? $request->input('paymentId') : null;
+        $customerData = $this->setShippingInfo($request);
+
+        $this->createPurchase($transactionId, $sessionToken, $customerData, 'complete', $cartItems, $cartItems->price, 'paypal');
+
+        $this->fullfillmentEmail($customerData->shipping, $cartItems);
+
+        return view('receipt', ['customerData' => $customerData, 'cartItems' => $cartItems, 'total' => $displayTotal]);
     }
 
 
