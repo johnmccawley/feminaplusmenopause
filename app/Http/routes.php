4648c68289bb46fa3d1be61f388b6987e390d53d
Sean Baluha <sbaluha@jekyllhydelabs.com>
2016-08-19 14:26:09 -0400

    Change notify route back to post

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -54,7 +54,7 @@ Route::post('/coupon/{id}/update', 'CouponController@update');
 
 // Subscription routes
 Route::post('/subscription', 'SubscriptionController@destroy');
-Route::any('/notify', 'SubscriptionController@store');
+Route::post('/notify', 'SubscriptionController@store');
 
 Route::get('/terms', function () {
     return view('terms');
