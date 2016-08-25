4b3821742ddfc701f7a8a5ba9e40ac1b64a584e1
Sean Baluha <sbaluha@jekyllhydelabs.com>
2016-08-25 17:09:04 -0400

    Fix Terms and Privacy page

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -13,8 +13,8 @@
     <div class="container">
         <div class="row">
             <h1>Terms & Conditions</h1>
-            <p>The content and images contained on the Femina Plus website are to be used for informational purposes only. All content and images on this website are the sole property of Helios Corp. and its licensees. Any use of content found on this site for commercial or resale purposes is strictly prohibited and may violate copyright, trademark and other laws. This information is intended for U.S. patients only.</p>
-            <p>Helios Corp. reserves the right to modify these terms and conditions at any time. Please review these terms and conditions periodically for revisions and updates.</p>
+            <p>The content and images contained on the Femina Plus website are to be used for informational purposes only. All content and images on this website are the sole property of Femina Plus and its licensees. Any use of content found on this site for commercial or resale purposes is strictly prohibited and may violate copyright, trademark and other laws. This information is intended for U.S. patients only.</p>
+            <p>Femina Plus reserves the right to modify these terms and conditions at any time. Please review these terms and conditions periodically for revisions and updates.</p>
             <p>The use of this site is considered as acceptance of these terms and conditions.</p>
         </div>
     </div>
