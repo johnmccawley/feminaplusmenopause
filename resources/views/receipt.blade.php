c0f8feb41e6cb6c8f02200e8307731c46a2d3ca4
Sean Baluha <sbaluha@jekyllhydelabs.com>
2016-09-13 16:38:34 -0400

    Changed receipt google analytics code to hopefully now work

--- resources/views/receipt.blade.php
+++ resources/views/receipt.blade.php
@@ -97,23 +97,42 @@
                 var total = $('#receiptTotal').text();
                 var totalSplit = total.split('$');
                 fbq('track', 'Purchase', {value: totalSplit[1], currency:'USD'});
-                
-                window.dataLayer = window.dataLayer || [];
+
+                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                ga('create', 'UA-77053752-1', 'auto');
+
+                var transId = $('#cart').data('transId');
                 var cart = $('#cart').data('cart');
-                var transProds = [];
+                ga('ecommerce:addTransaction', {
+                    'id': transId, // Transaction ID. Required.
+                    'affiliation': 'Femina Plus Menopause', // store name.
+                    'revenue': totalSplit[1] // total revenue.
+                    });
+
                 $.each(cart, function(index, value) {
-                    transProds.push({
-                        'sku': index,
-                        'name': value.name,
-                        'price': (value.price/100),
-                        'quantity': value.amount
+                    ga('ecommerce:addItem', {
+                        'id': transId, // Transaction ID. Required. Same as in the transaction data.
+                        'name': value.name, // Product name. Required.
+                        'sku': index, // Product SKU.
+                        'price': (value.price/100), // Product price.
+                        'quantity': value.amount // Product Quantity/
                     });
                 });
-                dataLayer.push({
-                   'transactionId': $('#cart').data('transId'),
-                   'transactionTotal': parseInt(totalSplit[1]),
-                   'transactionProducts': transProds
-                });
+
+                ga('ecommerce:send');
+
+                ga('send', 'pageview');
+
+                // window.dataLayer = window.dataLayer || [];
+                // dataLayer.push({
+                //    'transactionId': $('#cart').data('transId'),
+                //    'transactionTotal': parseInt(totalSplit[1]),
+                //    'transactionProducts': transProds
+                // });
             });
         </script>
     @endif
