820363d8da855ade4a900b7f1ad4043bb53f4fcc
Sean Baluha <sbaluha@jekyllhydelabs.com>
2016-10-04 15:54:48 -0400

    Move analytics code to footer

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -9,7 +9,6 @@
 
     <title>@yield('page_title') | Femina Plus</title>
     <link href="{{ url('/css/style.css') }}" rel="stylesheet">
-    {{-- <script type="text/javascript" src="{{ url('/js/all.js') }}"></script> --}}
 
     <link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-touch-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-touch-icon-60x60.png">
@@ -34,50 +33,6 @@
     <meta name="theme-color" content="#ffffff">
 
     @yield('styles')
-
-    @if(env('APP_ENV') == 'production')
-        <!-- Facebook Pixel Code -->
-        <script>
-            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
-                n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
-                t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
-                    document,'script','https://connect.facebook.net/en_US/fbevents.js');
-
-            fbq('init', '628465503996501');
-        </script>
-        <noscript>
-            <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=628465503996501&ev=PageView&noscript=1"/>
-        </noscript>
-        <!-- End Facebook Pixel Code -->
-
-        @if ($page_id != "receipt")
-            <!-- Google Analytics Code -->
-            <script>
-                (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-                            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-                ga('create', 'UA-77053752-1', 'auto');
-                ga('send', 'pageview');
-            </script>
-            <!-- End Google Analytics Code -->
-        @endif
-
-        <!-- Hotjar Tracking Code for https://feminaplusmenopause.com/ -->
-        <script>
-            (function(h,o,t,j,a,r){
-                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-                h._hjSettings={hjid:292068,hjsv:5};
-                a=o.getElementsByTagName('head')[0];
-                r=o.createElement('script');r.async=1;
-                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-                a.appendChild(r);
-            })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
-        </script>
-
-    @endif
 
 </head>
 
@@ -211,21 +166,66 @@
                         </div>
                     </div>
                 </div>
+
+                <script type="text/javascript">
+                    function downloadJSAtOnload() {
+                        var element = document.createElement("script");
+                        element.src = "{{ url('/js/all.js') }}";
+                        document.body.appendChild(element);
+                    }
+                    if (window.addEventListener)
+                        window.addEventListener("load", downloadJSAtOnload, false);
+                    else if (window.attachEvent)
+                        window.attachEvent("onload", downloadJSAtOnload);
+                    else window.onload = downloadJSAtOnload;
+                </script>
+
+                @if(env('APP_ENV') == 'production')
+                    <!-- Facebook Pixel Code -->
+                    <script>
+                        !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                                n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+                            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+                            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+                                document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+                        fbq('init', '628465503996501');
+                    </script>
+                    <noscript>
+                        <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=628465503996501&ev=PageView&noscript=1"/>
+                    </noscript>
+                    <!-- End Facebook Pixel Code -->
+
+                    @if ($page_id != "receipt")
+                        <!-- Google Analytics Code -->
+                        <script>
+                            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                                        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                                    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+                            ga('create', 'UA-77053752-1', 'auto');
+                            ga('send', 'pageview');
+                        </script>
+                        <!-- End Google Analytics Code -->
+                    @endif
+
+                    <!-- Hotjar Tracking Code for https://feminaplusmenopause.com/ -->
+                    <script>
+                        (function(h,o,t,j,a,r){
+                            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                            h._hjSettings={hjid:292068,hjsv:5};
+                            a=o.getElementsByTagName('head')[0];
+                            r=o.createElement('script');r.async=1;
+                            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                            a.appendChild(r);
+                        })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+                    </script>
+
+                @endif
             </footer>
             @yield('scripts')
         </div>
     </div>
-    <script type="text/javascript">
-        function downloadJSAtOnload() {
-            var element = document.createElement("script");
-            element.src = "{{ url('/js/all.js') }}";
-            document.body.appendChild(element);
-        }
-        if (window.addEventListener)
-            window.addEventListener("load", downloadJSAtOnload, false);
-        else if (window.attachEvent)
-            window.attachEvent("onload", downloadJSAtOnload);
-        else window.onload = downloadJSAtOnload;
-    </script>
 </body>
 </html>
